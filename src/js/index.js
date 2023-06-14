6ac5c5e0e7c88643e6a89c91f8ddad18c3652f2e
Artur Novikov <art14ek@tut.by>
2023-06-14 18:02:34 +0300

    second commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,71 @@
 import '../scss/style.scss'
 
-console.log('Works!')
+import Swiper, { Navigation, Pagination } from 'swiper'
+Swiper.use([Navigation, Pagination])
+;('use strict')
+
+const showMoreBtnFunc = function () {
+  let showMore = document.querySelector('.services__show-more-btn')
+
+  showMore.addEventListener('click', function () {
+    let showMoreBtn = document.querySelector('.services__show-more-btn')
+    let servicesMenu = document.querySelector('.services__menu')
+
+    if (!showMoreBtn.classList.contains('open', 'btn--rotate')) {
+      showMoreBtn.classList.add('open', 'btn--rotate')
+      showMoreBtn.textContent = 'Скрыть'
+      servicesMenu.classList.add('services__menu--open')
+      servicesMenu.classList.remove('services__menu--close')
+    } else {
+      showMoreBtn.classList.remove('open', 'btn--rotate')
+      showMoreBtn.textContent = 'Показать все'
+      servicesMenu.classList.add('services__menu--close')
+      servicesMenu.classList.remove('services__menu--open')
+    }
+  })
+}
+showMoreBtnFunc()
+
+let swiperSlideAll = document.querySelectorAll('.services__element')
+let containerMenu = document.querySelector('.container__menu')
+let servicesMenu = document.querySelector('.services__menu')
+let x = window.matchMedia('(max-width: 767px)')
+function myFunction(x) {
+  if (x.matches) {
+    for (let i = 0; i < swiperSlideAll.length; i++) {
+      if (!swiperSlideAll[i].classList.contains('swiper-slide')) {
+        swiperSlideAll[i].classList.add('swiper-slide')
+      }
+    }
+    if (!servicesMenu.classList.contains('swiper-wrapper')) {
+      servicesMenu.classList.add('swiper-wrapper')
+    }
+    if (!containerMenu.classList.contains('swiper')) {
+      containerMenu.classList.add('swiper')
+    }
+
+    let swiper = new Swiper('.swiper', {
+      spaceBetween: 16,
+      slidesPerView: 1.18,
+      allowTouchMove: true,
+      speed: 400,
+      height: 72,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+      }
+    })
+  } else {
+    let swiper = new Swiper('.swiper', {})
+    swiper.destroy()
+    for (let i = 0; i < swiperSlideAll.length; i++) {
+      if (swiperSlideAll[i].classList.contains('swiper-slide')) {
+        swiperSlideAll[i].classList.remove('swiper-slide')
+      }
+    }
+    servicesMenu.classList.remove('swiper-wrapper')
+    containerMenu.classList.remove('swiper')
+  }
+}
+myFunction(x)
+x.addEventListener('change', myFunction)
