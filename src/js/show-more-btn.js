527672c2e98a5321547d0651f7291dffeb559368
Artur Novikov <art14ek@tut.by>
2023-06-16 23:08:54 +0300

    кнопки открыть все работают все

--- src/js/show-more-btn.js
+++ src/js/show-more-btn.js
@@ -1,31 +1,36 @@
-// const showMoreBtnFunc = function () {
-//   let showMoreAll = document.querySelectorAll('.services__show-more-btn')
-//   console.log(showMoreAll)
-//   for (let i = 0; i < showMoreAll.length; i++) {
-//     let showBtn = showMoreAll[i]
-//     console.log(showBtn)
+let buttonsListener = document.querySelectorAll('.services__show-more-btn')
 
-//     /* let showMore = document.querySelector('.services__show-more-btn') */
+buttonsListener[0].addEventListener('click', function () {
+  let showMoreBtn = document.querySelector('.services__show-more-btn')
+  let servicesMenu = document.querySelector('.services__menu')
 
-//     /* showMore */ showBtn.addEventListener('click', function () {
-//       let showMoreBtn = document.querySelector('.services__show-more-btn')
-//       let servicesMenu = document.querySelector('.services__menu')
-
-//       if (!showMoreBtn.classList.contains('open', 'btn--rotate')) {
-//         showMoreBtn.classList.add('open', 'btn--rotate')
-//         showMoreBtn.textContent = 'Скрыть'
-//         servicesMenu.classList.add('services__menu--open')
-//         servicesMenu.classList.remove('services__menu--close')
-//       } else {
-//         showMoreBtn.classList.remove('open', 'btn--rotate')
-//         showMoreBtn.textContent = 'Показать все'
-//         servicesMenu.classList.add('services__menu--close')
-//         servicesMenu.classList.remove('services__menu--open')
-//       }
-//     })
-//   }
-// }
-// showMoreBtnFunc()
+  if (!showMoreBtn.classList.contains('open', 'btn--rotate')) {
+    showMoreBtn.classList.add('open', 'btn--rotate')
+    showMoreBtn.textContent = 'Скрыть'
+    servicesMenu.classList.add('services__menu--open')
+    servicesMenu.classList.remove('services__menu--close')
+  } else {
+    showMoreBtn.classList.remove('open', 'btn--rotate')
+    showMoreBtn.textContent = 'Показать все'
+    servicesMenu.classList.add('services__menu--close')
+    servicesMenu.classList.remove('services__menu--open')
+  }
+})
+buttonsListener[1].addEventListener('click', function () {
+  let showMoreBtn = document.querySelector('.services__show-more-btn-types')
+  let servicesMenu = document.querySelector('.types__menu')
+  if (!showMoreBtn.classList.contains('open', 'btn--rotate')) {
+    showMoreBtn.classList.add('open', 'btn--rotate')
+    showMoreBtn.textContent = 'Скрыть'
+    servicesMenu.classList.add('services__menu--open')
+    servicesMenu.classList.remove('services__menu--close')
+  } else {
+    showMoreBtn.classList.remove('open', 'btn--rotate')
+    showMoreBtn.textContent = 'Показать все'
+    servicesMenu.classList.add('services__menu--close')
+    servicesMenu.classList.remove('services__menu--open')
+  }
+})
 
 let aboutText = document.querySelector('.about__text')
 let showButton = document.querySelector('.about__show-more-btn')
