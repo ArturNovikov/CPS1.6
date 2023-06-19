fec2c6af594ee03ef899fa8de82de73b069552b0
Artur Novikov <art14ek@tut.by>
2023-06-19 18:47:34 +0300

    запилил движок для бургера(html, js, scss, MRQST

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,1 +1,35 @@
 console.log('Hello!')
+
+/* работает до ширины экрана 1439 px */
+
+/* aside-menu--hidden - класс для toggle */
+/* 
+вашаем обработчие события на меню бургера
+
+клик по бургеру - запускается проверка условия
+
+если ширина экрана меньше 1439px тогда запускаеться 
+
+classList.toggle('aside-menu--hidden');
+
+ */
+
+let maxWidth = window.matchMedia('(max-width: 1439px)')
+let headerBurger = document.querySelector('.header__burger')
+let asideMenu = document.querySelector('.aside-menu')
+let buttonCross = document.querySelector('.aside-menu__btn-cross')
+let containerHidden = document.querySelector('.aside__container--hidden')
+
+headerBurger.onclick = function () {
+  if (maxWidth) {
+    asideMenu.classList.toggle('aside-menu--hidden')
+    containerHidden.classList.toggle('aside__container--hidden')
+  }
+}
+
+buttonCross.onclick = function () {
+  if (maxWidth) {
+    asideMenu.classList.toggle('aside-menu--hidden')
+    containerHidden.classList.toggle('aside__container--hidden')
+  }
+}
